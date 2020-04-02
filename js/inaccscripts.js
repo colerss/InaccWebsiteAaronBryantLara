b9be8214f16bd369cc5bf9e51114d35dcbfa3292
Bryant <bryantsuiskensmast@gmail.com>
2020-04-02 13:25:11 +0200

    Update inaccscripts.js

--- js/inaccscripts.js
+++ js/inaccscripts.js
@@ -5,4 +5,5 @@ function setHeight() {
     let currentHeight = document.getElementById("headerheight").clientHeight;
 
     document.getElementById("flowheight").style.height = currentHeight +"px";
+    window.scrollTo(0, 0); //zorgt er voor dat de scroll niet naar beneden word geduwd
 }
