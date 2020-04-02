c1737c753c5f97f6554df4417f6939dee6f31bf9
Bryant Suiskens <61180866+colerss@users.noreply.github.com>
2020-04-02 17:02:15 +0200

    Vestiging fixes
    Fixes voor Lara

--- js/inaccscripts.js
+++ js/inaccscripts.js
@@ -1,5 +1,6 @@
 
 setHeight();
+myMap();
 
 function setHeight() {
     let currentHeight = document.getElementById("headerheight").clientHeight;
@@ -7,3 +8,11 @@ function setHeight() {
     document.getElementById("flowheight").style.height = currentHeight +"px";
     window.scrollTo(0, 0); //zorgt er voor dat de scroll niet naar beneden word geduwd
 }
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
