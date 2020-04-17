9190bb669ea329e350f68b0eb4144f31aa7063b0
Bryant <bryantsuiskensmast@gmail.com>
2020-04-17 10:34:48 +0200

    Prototype overlay

--- js/inaccscripts.js
+++ js/inaccscripts.js
@@ -59,3 +59,14 @@ function myMap() {
               return testingMail.test(String(email).toLowerCase());
             }
             
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
