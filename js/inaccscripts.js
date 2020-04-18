79a2005aa6796d52eb285f3ad4d485ec3e1d93db
Bryant <bryantsuiskensmast@gmail.com>
2020-04-18 23:26:17 +0200

    commentaar toegevoegt

--- js/inaccscripts.js
+++ js/inaccscripts.js
@@ -5,7 +5,8 @@ myMap();
 function setHeight() {
     let currentHeight = document.getElementById("headerheight").clientHeight;
 
-    document.getElementById("flowheight").style.height = currentHeight +"px";
+    document.getElementById("flowheight").style.height = currentHeight +"px"; /*Doet er dynamisch voor zorgen dat ongeacht de 
+    schermgroote waarop de pagina gerenderd word, de placeholder ruimte voor de header altijd gelijk is aan de header*/
     window.scrollTo(0, 0); //zorgt er voor dat de scroll niet naar beneden word geduwd
 }
 
